61e920a4e967c97cc05528842f48239b589875dd
gruber86 <mateusz.roman@stxnext.pl>
2014-03-14 15:46:49 +0100

    [#1511]Tabs fine, json not

--- src/presence_analyzer/views.py
+++ src/presence_analyzer/views.py
@@ -4,7 +4,7 @@ Defines views.
 """
 
 import calendar
-from flask import redirect
+from flask import redirect, render_template, redirect, url_for
 
 from presence_analyzer.main import app
 from presence_analyzer.utils import jsonify, get_data, mean, group_by_weekday, group_times_by_weekday
@@ -18,7 +18,31 @@ def mainpage():
     """
     Redirects to front page.
     """
-    return redirect('/static/presence_weekday.html')
+    return render_template('presence_weekday.html')
+
+
+@app.route('/presence-weekday')
+def presence_weekday():
+    """
+    Redirects to presence weekday page.
+    """
+    return render_template('presence_weekday.html')    
+
+
+@app.route('/mean-time-weekday')
+def mean_time_weekday():
+    """
+    Redirects to mean time weekday page.
+    """
+    return render_template('mean_time_weekday.html')
+
+
+@app.route('/presence-start-end')
+def presence_start_end():
+    """
+    Redirects to presence start end page.
+    """
+    return render_template('presence_start_end.html')
 
 
 @app.route('/api/v1/users', methods=['GET'])
