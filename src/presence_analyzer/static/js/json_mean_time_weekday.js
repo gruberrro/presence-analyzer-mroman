feaba732bd3317b33ee81479fb98a5cc429facac
gruber86 <mateusz.roman@stxnext.pl>
2014-03-27 15:15:04 +0100

    [#1631] Table has no column err

--- src/presence_analyzer/static/js/json_mean_time_weekday.js
+++ src/presence_analyzer/static/js/json_mean_time_weekday.js
@@ -19,32 +19,39 @@ function parseInterval(value) {
         });
         $('#user_id').change(function(){
             var selected_user = $("#user_id").val();
-            var avatar = $('#user_id').data('avatar')
+            var avatar = $('#user_id').data('avatar');
             var chart_div = $('#chart_div');
             if(selected_user) {
                 var newImage = users[selected_user]['avatar'];
+                var user = users[selected_user]['user_id'];
                 $('#avatar').children('img').attr('src', newImage);
                 loading.show();
                 chart_div.hide();
-                $.getJSON("/api/v1/mean_time_weekday/"+selected_user, function(result) {
-                    $.each(result, function(index, value) {
+                $.getJSON("/api/v1/mean_time_weekday/"+user, function(result) {
+                    if(result.length !==0) {
+                        $.each(result, function(index, value) {
                         value[1] = parseInterval(value[1]);
-                    });
-                    console.log(result);
-                    var data = new google.visualization.DataTable();
-                    data.addColumn('string', 'Weekday');
-                    data.addColumn('datetime', 'Mean time (h:m:s)');
-                    data.addRows(result);
-                    var options = {
-                        hAxis: {title: 'Weekday'}
-                    };
-                    var formatter = new google.visualization.DateFormat({pattern: 'HH:mm:ss'});
-                    formatter.format(data, 1);
+                        });
+                        var data = new google.visualization.DataTable();
+                        data.addColumn('string', 'Weekday');
+                        data.addColumn('datetime', 'Mean time (h:m:s)');
+                        data.addRows(result);
+                        var options = {
+                            hAxis: {title: 'Weekday'}
+                        };
+                        var formatter = new google.visualization.DateFormat({pattern: 'HH:mm:ss'});
+                        formatter.format(data, 1);
 
-                    chart_div.show();
-                    loading.hide();
-                    var chart = new google.visualization.ColumnChart(chart_div[0]);
-                    chart.draw(data, options);
+                        chart_div.show();
+                        loading.hide();
+                        var chart = new google.visualization.ColumnChart(chart_div[0]);
+                        chart.draw(data, options);  
+                    }
+                    else{
+                        chart_div.show();
+                        $('#chart_div').text('Brak danych dla tego użytkownika');
+                        loading.hide();
+                    }
                 });
             }
         });
